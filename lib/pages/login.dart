c0834665b8adb5b5acdfb005085cd60f6b19569c
wincentrtz <wincentang3636@gmail.com>
2019-01-20 23:06:04 +0700

    initial commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
